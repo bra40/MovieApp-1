1c599e7b33714bb2ab60a7c2288458ce0c437ba5
jsweeney12 <jakemsweeney12@gmail.com>
2022-03-15 15:44:53 -0400

    Flask and React can talk to eachother

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import UserPage from './Components/UserPage/UserPage';
 
 function App() {
-  return (
-    <div className="App">
-      <UserPage/>
-    </div>
-  );
+
+  // used to store and set data that is recieved from flask
+  const [data, setData] = useState(0);
+
+    // makes sure the flask backend is up and running
+    useEffect(() => {
+        fetch("/movieapp")
+        .then(
+            res => res.status
+        )
+        .then(
+            data => {
+                setData(data)
+            }
+        )
+    }, [])
+
+    return (
+        <div>
+            {(data !== 201) ? (
+                <p>ERROR: {data}</p>
+            ) : (
+                <div>
+                    Success!
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/Components/UserPage/UserPage.jsx
+++ frontend/src/Components/UserPage/UserPage.jsx
@@ -3,11 +3,10 @@ import React, { Component } from 'react'
 export default class UserPage extends Component {
   render() {
     return (
-      <div className = "userPage">
-          <h1>
-              Your Profile
-          </h1>
+      <div>
+        UserPage
       </div>
     )
   }
 }
+
